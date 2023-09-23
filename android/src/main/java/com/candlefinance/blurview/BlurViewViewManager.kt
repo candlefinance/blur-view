2b7a135c02ed5040dda90e4d8780e4e7c1bb1c7c
Gary Tokman <hello@garytokman.me>
2023-09-23 00:13:05 -0600

    fix(android): crash

--- android/src/main/java/com/candlefinance/blurview/BlurViewViewManager.kt
+++ android/src/main/java/com/candlefinance/blurview/BlurViewViewManager.kt
@@ -19,7 +19,7 @@ class BlurViewViewManager : SimpleViewManager<BlurView>() {
     val blurView = BlurView(reactContext.baseContext).also {
       it.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
       val decorView = reactContext.currentActivity
-      val rootView = decorView?.findViewById<BlurView>(R.id.blurView)
+      val rootView = decorView?.findViewById(android.R.id.content) as? ViewGroup
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         if (rootView != null) {
           it.setupWith(rootView, RenderEffectBlur())
